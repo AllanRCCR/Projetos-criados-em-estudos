f7c62667c21485b0a6da377d84a0a3adfabd77be
AllanRCCR <allan.rcrc@gmail.com>
2023-01-11 18:11:27 -0300

    atualização dos projetos em andamento

---
+++ primeiro codigo em js/aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,43 @@
+function playSound (selectorAudio) {
+    const element = document.querySelector(selectorAudio);
+
+
+    if(element != null && element.localName === 'audio'){
+            element.play(); 
+    }
+
+    else{
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+}
+
+
+
+const keyList = document.querySelectorAll('.tecla');
+
+
+
+for (let counter = 0; counter < keyList.length; counter++) {
+
+    const key = keyList[counter];
+
+    const instrument = key.classList[1];
+
+    const idAudio = `#som_${instrument}`; //template string
+
+    key.onclick = function(){
+        playSound(idAudio);
+    }
+
+    key.onkeydown = function (event) {
+
+        if (event.code === 'Space' || event.code === 'Enter') {
+            key.classList.add('ativa');
+        }
+    }
+
+    key.onkeyup = function () {
+        key.classList.remove('ativa');
+    }
+ 
+}
